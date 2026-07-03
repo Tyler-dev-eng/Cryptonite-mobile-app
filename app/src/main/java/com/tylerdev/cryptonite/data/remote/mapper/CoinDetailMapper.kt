29f217c88838a1382207605074daeaad0950945e
tyleravochoc <tyler@avochoc.com>
2026-07-03 17:50:32 +0200

    Add coin detail domain mapper
    Implement the `CoinDetailDto` to `CoinDetailDomainModel` mapper so remote coin detail responses can be converted into the domain layer model, including tag name extraction and team data mapping.

--- app/src/main/java/com/tylerdev/cryptonite/data/remote/mapper/CoinDetailMapper.kt
+++ app/src/main/java/com/tylerdev/cryptonite/data/remote/mapper/CoinDetailMapper.kt
@@ -1,1 +1,18 @@
 package com.tylerdev.cryptonite.data.remote.mapper
+
+import com.tylerdev.cryptonite.data.remote.dto.CoinDetailDto
+import com.tylerdev.cryptonite.domain.model.CoinDetailDomainModel
+import com.tylerdev.cryptonite.domain.model.CoinDomainModel
+
+fun CoinDetailDto.toCoinDetailDomainModel(): CoinDetailDomainModel {
+    return CoinDetailDomainModel (
+        coinId = id,
+        name = name,
+        description = description,
+        symbol = symbol,
+        rank = rank,
+        isActive = isActive,
+        tags = tags.map { it.name },
+        team = team
+    )
+}
